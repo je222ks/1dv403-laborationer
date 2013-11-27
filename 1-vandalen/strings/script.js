7dc825091031605fdd0af901bda69167e1410719
je222ks <je222ks@student.lnu.se>
2013-11-27 23:44:35 +0100

    Strängar - Problem kvarstår

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -4,16 +4,40 @@ window.onload = function(){
 
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var convertString = function(str){
-		// Plats för förändring.		
+        // var answer;
+		var i;
+        var answer;
+		
+		str.split();
+		
+		for(i = 0; i < str.length; i += 1){
+			console.log("Hej Hopp");
+            
+            if(str.charAt(i) === str.charAt(i).toLocaleLowerCase()){
+                str += str.charAt(i).toLocaleUpperCase();
+            }
+            else{
+                str += str.charAt(i).toLocaleLowerCase();
+            }
+			
+			str = str.replace("A", "#");
+			str = str.replace("a", "#");
+		
+            answer = str.concat();
+        }
+		
 		// Returnera den konverterade strängen.
+		return answer;
+		
 		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
-	
-
-
-
-
-
-
+		try{
+			if(str === null || str.length < 0){
+				throw new Error("Du har inte angivit en giltig sats.");
+			}
+		}
+		catch(Error){
+			alert(Error.message);
+		}
 	};
 	// ------------------------------------------------------------------------------
 
