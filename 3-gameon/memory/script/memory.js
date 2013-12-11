97c8560fbc87940043a2d6fc95bb5e9ed350fa62
je222ks <je222ks@student.lnu.se>
2013-12-12 00:56:30 +0100

    "rendering" av bilderna

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,10 +2,31 @@
 
 var Memory = {
     
-    init : function () {
-        alert("Hej!");
-    }
+    playBoard : null, 
+
+    init : function () {    
+        var rowInput = 4;
+        var colInput = 3;
+        
+        Memory.playBoard = RandomGenerator.getPictureArray(rowInput, colInput);
+        Memory.renderBoard();
+    },
     
+    renderBoard : function () {
+        var memoryContainer = document.createElement("div");
+        var memoryImg;
+        var imgSrc;
+        
+        for (var i = 0; i < Memory.playBoard.length; i +=1 ){
+            imgSrc = "pics/" + Memory.playBoard[i] + ".png";
+            memoryImg = document.createElement("img");
+            memoryImg.setAttribute("src", imgSrc);   
+                
+            memoryContainer.appendChild(memoryImg);
+        }
+        
+        document.body.appendChild(memoryContainer);
+    }
 };
               
 window.onload = Memory.init;
