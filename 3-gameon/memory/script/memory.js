f84c550befe94d2c0f3fc660c5308d7de9a372f7
je222ks <je222ks@student.lnu.se>
2013-12-12 01:19:26 +0100

    lättare styling

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -6,22 +6,24 @@ var Memory = {
 
     init : function () {    
         var rowInput = 4;
-        var colInput = 3;
+        var colInput = 4;
         
         Memory.playBoard = RandomGenerator.getPictureArray(rowInput, colInput);
         Memory.renderBoard();
     },
     
     renderBoard : function () {
+        var i;
         var memoryContainer = document.createElement("div");
+        memoryContainer.className = "gamecontainer";
         var memoryImg;
         var imgSrc;
         
-        for (var i = 0; i < Memory.playBoard.length; i +=1 ){
+        for (i = 0; i < Memory.playBoard.length; i +=1 ){
             imgSrc = "pics/" + Memory.playBoard[i] + ".png";
             memoryImg = document.createElement("img");
             memoryImg.setAttribute("src", imgSrc);   
-                
+              
             memoryContainer.appendChild(memoryImg);
         }
         
