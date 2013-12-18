29fcdb16c79f5967d0448aef553b71781ac2f9ae
je222ks <je222ks@student.lnu.se>
2013-12-18 23:21:38 +0100

    påbörjat en bröjan till validering efter ett flertal misslyckade försök...

---
+++ 4-forms/validator.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+var Validator = {
+
+    // "Laddar in" formuläret
+    form : document.getElementById("payform"),
+    
+    firstName : document.forms[0]["firstName"],
+    lastName : document.forms[0]["lastName"],
+    
+    init : function () {
+        
+    }
+};
+    
+    console.log(Validator.form);
+    console.log(Validator.firstName);
+    console.log(Validator.lastName);
+    
+
+
