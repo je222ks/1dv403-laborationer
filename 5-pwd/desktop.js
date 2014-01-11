778fbf2f688e72de8884671eeef81d45306ffa8a
je222ks <je222ks@student.lnu.se>
2014-01-11 16:47:28 +0100

    påbörjat kodningen av projektet - basic setup

---
+++ 5-pwd/desktop.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function Desktop() {
+    
+    var mainBG = document.createElement("div");
+    mainBG.setAttribute("id", "mainBG");
+    document.body.appendChild(mainBG);
+
+}
---
+++ 5-pwd/engine.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var Application = {
+    
+    init : function () {
+        var desktop = new Desktop();
+    }
+    
+};
+
+window.onload = Application.init;
