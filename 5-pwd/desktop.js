14cded86072bb89549aced1aa2a5e428121353ba
je222ks <je222ks@student.lnu.se>
2014-01-11 21:24:38 +0100

    mindre ändringar då jag ångrade mig och basic layout för desktop

--- 5-pwd/desktop.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function Desktop() {
-    
-    var mainBG = document.createElement("div");
-    mainBG.setAttribute("id", "mainBG");
-    document.body.appendChild(mainBG);
-
-}
--- 5-pwd/engine.js
+++ 5-pwd/engine.js
@@ -3,9 +3,35 @@
 var Application = {
     
     init : function () {
-        var desktop = new Desktop();
+        Desktop.renderDesktop();
+    }
+    
+};
+
+
+var Desktop = {
+    
+    renderDesktop : function () {
+        var mainBG = document.createElement("div");
+        mainBG.setAttribute("id", "mainBG");
+        
+        var menuField = document.createElement("div");
+        menuField.setAttribute("id", "menufield");
+        
+        var menuChoice = document.createElement("a");
+        menuChoice.setAttribute("href", "#");
+        var menuThumb = document.createElement("img");
+        menuThumb.setAttribute("src", "imgthumb.png");
+        menuThumb.setAttribute("id", "menuthumb");
+        menuChoice.appendChild(menuThumb);
+        
+        menuField.appendChild(menuChoice);
+        mainBG.appendChild(menuField);
+        document.body.appendChild(mainBG);
     }
     
 };
 
 window.onload = Application.init;
+
+
